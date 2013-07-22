4d5a3711d01b53ed8f0aa17876c18e42cc2b683c
Jason Held <jsonheld@gmail.com>
2013-07-22 00:45:17 -0400

    "Upload" file, if pass go to success page.

--- tarme/compression/views.py
+++ tarme/compression/views.py
@@ -10,10 +10,26 @@ from django.views.generic import ListView
 from compression.models import Document
 from compression.forms import DocumentForm
 
-class CompressView(ListView):
-    #template_name = 'compress.html'
-    model = Document
-    '''
+class CompressView(FormView):
+    template_name = 'compress.html'
+    success_url = '/success/'
     form_class = DocumentForm
-    success_url = '/compressed/'
+
+    def form_valid(self, form):
+        return super(CompressView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        return super(CompressView, self).form_invalid(form)
+
     '''
+    def post(self, request, *args, **kwargs):        
+        form = DocumentForm(request.POST,request.FILES)
+        if self.form_valid(form):
+            return self.render_to_response(self.get_context_data(form=
+    '''
+    
+    def get_context_data(self, **kwargs):
+        context = super(CompressView, self).get_context_data(**kwargs)
+        return context
+
+
--- tarme/tarme/urls.py
+++ tarme/tarme/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 from compression.views import CompressView
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
@@ -12,9 +13,10 @@ urlpatterns = patterns('',
     # url(r'^tarme/', include('tarme.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-url('^$',CompressView.as_view(),name='compress'),
+    #url(r'^admin/', include(admin.site.urls)),
+                       url('^success/*$',TemplateView.as_view(template_name='success.html'),name='success'),
+url('^\w*$',CompressView.as_view(),name='compress'),
 )
