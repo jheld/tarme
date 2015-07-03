f1b0344b1c5aeb42a03ce1afe5cf8a050d4b1df5
Jason Held <jsonheld@gmail.com>
2015-07-03 16:42:11 -0400

    adds a index/home page.

--- tarme/tarme/urls.py
+++ tarme/tarme/urls.py
@@ -19,6 +19,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
                        url('^success/{0,1}$',SuccessView.as_view(),name='success'),
+                       url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
 #url('^compression_type/$',TemplateView.as_view(template_name='compression_type.html'),name='compression_type'),
 url('^document_list/(?P<pk>\d+)/compress$',CompressView.as_view(),name='compress'),
 url('^document_list/*$',ListView.as_view(template_name='compression/document_list.html',model=Document),name='document_list'),
